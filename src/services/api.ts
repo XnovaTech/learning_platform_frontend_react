e936eab19bbcd1f7bc5ac6fc20f97b8f1277f7c1
Nyan Lin Htet <nyanlinhtet206@gmail.com>
2025-11-22 22:18:07 +0630

    add env for api

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
     //baseURL: 'https://api.thecrystalacademy.org/api',
-    baseURL: 'http://127.0.0.1:8000/api',
+    baseURL: import.meta.env.VITE_APP_API_URL,
     withCredentials: true,
     headers: {
         'Accept': 'application/json'
